4d9f184a16e892c6ea115c3730fc5d1a7cf373a7
Suslov Yury <susloff.yury@gmail.com>
2020-03-05 10:03:28 +0300

    first_concept

---
+++ new-app/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './style'
+
+
+function Header() {
+
+    return (
+        <header style={style} > This is Header </header>
+    )
+}
+export default Header;
