09c3fe48a49cd2ce366aa453070369a0bcf8628e
Suslov Yury <susloff.yury@gmail.com>
2020-03-05 10:12:16 +0300

    First_concept

---
+++ new-app/src/style.js
@@ -0,0 +1,11 @@
+const style = {
+    color: "#ffffff",
+height: "100px",
+ fontSize: '20px',
+ backgroundColor:'#282c34',
+ display: "flex",
+ justifyContent: "center",
+ alignItems: 'center'
+}
+
+export default style;
